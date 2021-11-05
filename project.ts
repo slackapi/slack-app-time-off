b195ee18fe9549fe6b5760e7c2f686ebaf28f7d3
Pamela Gluss <pgluss@slack-corp.com>
2021-11-05 16:04:38 -0700

    ignore this

--- project.ts
+++ project.ts
@@ -4,7 +4,7 @@ import { TimeOffWorkflow } from "./workflows/time_off_workflow.ts";
 import { TimeOffShortcut } from "./triggers/time_off_shortcut.ts";
 
 Project({
-  name: "Time-off Request",
+  name: "Time-off Request TEST",
   description: "A very basic time-off request app",
   icon: "assets/icon.png",
   runtime: "deno1.x",
